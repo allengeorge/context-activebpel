3cca3a3e795deb5a88894dd4599e4320fd3c95d8
allen.george <allen.george@9a138582-d5b3-11dd-9aad-3f7dfcff7c77>
2008-12-30 23:48:08 +0000

    Initial import of code-tree folders

---
+++ trunk/activebpel-4.1/projects/org.activebpel.rt.axis.bpel.web/src/org/activebpel/rt/axis/bpel/web/AeBuildNumber.java
@@ -0,0 +1,76 @@
+//$Header: /Development/AEDevelopment/projects/org.activebpel.rt.axis.bpel.web/src/org/activebpel/rt/axis/bpel/web/AeBuildNumber.java,v 1.3 2005/02/01 19:47:4
+/*
+ * Copyright (c) 2004-2006 Active Endpoints, Inc.
+ *
+ * This program is licensed under the terms of the GNU General Public License
+ * Version 2 (the "License") as published by the Free Software Foundation, and 
+ * the ActiveBPEL Licensing Policies (the "Policies").  A copy of the License 
+ * and the Policies were distributed with this program.  
+ *
+ * The License is available at:
+ * http: *www.gnu.org/copyleft/gpl.html
+ *
+ * The Policies are available at:
+ * http: *www.activebpel.org/licensing/index.html
+ *
+ * Unless required by applicable law or agreed to in writing, this program is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied.  See the License and the Policies
+ * for specific language governing the use of this program.
+ */
+package org.activebpel.rt.axis.bpel.web;
+
+
+import java.io.*;
+import java.util.Properties;
+
+/**
+* Resource file which contains the build number and build date.
+*/
+public class AeBuildNumber
+{
+   private static String mBuildNumber = ""; //$NON-NLS-1$
+   private static String mBuildDate   = ""; //$NON-NLS-1$
+   
+   static
+   {
+      try
+      {
+         Properties props = new Properties();
+         props.load(new AeBuildNumber().getClass().getResourceAsStream("version.properties")); //$NON-NLS-1$
+         
+         mBuildNumber = props.getProperty("build.number"); //$NON-NLS-1$
+         mBuildDate   = props.getProperty("build.date"); //$NON-NLS-1$
+      }
+      catch (IOException ioe)
+      {
+      }
+   }
+   
+   /**
+    * default constructor for a build resource file
+    */
+   private AeBuildNumber()
+   {
+   }
+   
+   /**
+    * Obtains the build number for this component.
+    * 
+    * @return a String value representing the build number
+    */
+   public final static String getBuildNumber() 
+   {
+      return mBuildNumber;
+   }
+   
+   /**
+    * Obtains the build date for this component.
+    * 
+    * @return a String value representing the build date
+    */
+   public final static String getBuildDate() 
+   {
+      return mBuildDate;
+   }
+}
---
+++ trunk/activebpel-4.1/projects/org.activebpel.rt.axis.bpel.web/src/org/activebpel/rt/axis/bpel/web/AeMessages.java
@@ -0,0 +1,56 @@
+//$Header: /Development/AEDevelopment/projects/org.activebpel.rt.axis.bpel.web/src/org/activebpel/rt/axis/bpel/web/AeMessages.java,v 1.1 2005/02/08 15:31:4
+/*
+ * Copyright (c) 2004-2006 Active Endpoints, Inc.
+ *
+ * This program is licensed under the terms of the GNU General Public License
+ * Version 2 (the "License") as published by the Free Software Foundation, and 
+ * the ActiveBPEL Licensing Policies (the "Policies").  A copy of the License 
+ * and the Policies were distributed with this program.  
+ *
+ * The License is available at:
+ * http: *www.gnu.org/copyleft/gpl.html
+ *
+ * The Policies are available at:
+ * http: *www.activebpel.org/licensing/index.html
+ *
+ * Unless required by applicable law or agreed to in writing, this program is
+ * distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS
+ * OF ANY KIND, either express or implied.  See the License and the Policies
+ * for specific language governing the use of this program.
+ */
+package org.activebpel.rt.axis.bpel.web;
+
+import java.util.MissingResourceException;
+import java.util.ResourceBundle;
+
+/**
+ * Accessor class for externalized strings
+ */
+public class AeMessages
+{
+   private static final String BUNDLE_NAME = "org.activebpel.rt.axis.bpel.web.messages";//$NON-NLS-1$
+
+   private static final ResourceBundle RESOURCE_BUNDLE = ResourceBundle.getBundle(BUNDLE_NAME);
+
+   private AeMessages()
+   {
+   }
+
+   /**
+    * Returns externalized string from resource bundle or key surrounded by exclamation points
+    * if not found.
+    * @param key - Key of string to return
+    * @return - String from bundle
+    */      
+   public static String getString(String key)
+   {
+      try
+      {
+         return RESOURCE_BUNDLE.getString(key);
+      }
+      catch (MissingResourceException e)
+      {
+         return '!' + key + '!';
+      }
+   }
+}
